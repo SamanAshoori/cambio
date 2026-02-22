3a0e0e07deacd01f36aa0dd8617c870231277555
Saman <saman.ashoori@outlook.com>
2026-02-22 22:58:20 +0000

    added testing.py for playet to try out the new methods for power cards

--- player_testing.py
+++ player_testing.py
@@ -1,1 +1,11 @@
 from player import Player
+import random
+
+deck = [i for i in range(54)]
+#Cambio has 52 cards + 2 jokers (ignore for now)
+
+random.shuffle(deck)
+
+p = Player([deck.pop() for _ in range(4)], "Player 1")
+
+print(p.get_card_rank(10))
